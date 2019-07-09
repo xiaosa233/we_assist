17e394a0a5781cccd950503d658eab21991fa755
xiaojian <xiaosa233@gmail.com>
2019-07-09 17:02:16 +0800

    add run thread

--- source/modules/io_mod/tcp_client.py
+++ source/modules/io_mod/tcp_client.py
@@ -1,5 +1,11 @@
 import tcp_client_base
-from tcp_system import tcp_system
+import threading
+import enum from Enum
+
+
+class eclient_state(Enum):
+    unvalid = 0
+    connecting = 1
 
 class tcp_client(tcp_client_base.tcp_client_base):
     def __init__(self, ip, port, connected_cb = None , error_cb = None, msg_cb = None):
@@ -11,15 +17,17 @@ class tcp_client(tcp_client_base.tcp_client_base):
         self.msg_cb = msg_cb
         self.is_connected = False
         self.run_thread = None
+        self.client_state = eclient_state.unvalid
 
     def connect(self):
         self.is_connected = True
-        tcp_system.static_get().add_client(self)
-        tcp_system.static_get().run()
+        if self.run_thread is None :
+            self.client_state = eclient_state.connecting
+            self.run_thread  = threading.Thread(target=self.client_loop)
+            self.run_thread.start()
 
     def close(self):
         self.is_connected = False
-        tcp_system.static_get().remove_client(self)
 
     def on_connected_cb(self):
         self.start_work()
@@ -32,9 +40,10 @@ class tcp_client(tcp_client_base.tcp_client_base):
             self.error_cb()
 
 
-    async def client_loop(self):
-
-        pass
+    def client_loop(self):
+        while self.is_connected:
+            if self.client_state == eclient_state.connecting:
+                connect_impl()
 
     #virtual
     def on_msg(self, data):
