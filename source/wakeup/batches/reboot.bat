d0b3179ff976ee78ae0d38d4a362097f046003f1
xiaosa233 <xiaosa233@gmail.com>
2019-08-14 13:45:24 +0800

    add ping checker in batchfiles

--- source/wakeup/batches/reboot.bat
+++ source/wakeup/batches/reboot.bat
@@ -2,5 +2,21 @@
 rem reboot.bat [pid]
 rem kill process
 taskkill /pid %1 -f
+
+rem check web connection
+echo "begin ping www.baidu.com"
+set ping_time=0
+:loop
+ping www.baidu.com
+set return_value=%ERRORLEVEL%
+set /A ping_time=ping_time+1
+IF NOT %return_value%==0 (
+    timeout 4
+    echo "ping times : %ping_time%"
+    goto loop
+)
+
+
+
 rem reboot now 
 start cmd /c python %2/main.py
--- source/wakeup/batches/reboot.sh
+++ source/wakeup/batches/reboot.sh
@@ -1,5 +1,20 @@
 # kill pid
 kill -s 9 $1
+
+#check ping 
+#ping
+return_code=1
+ping_times=0
+while [ $return_code -ne 0 ]; do
+        ping -c 4 www.baidu.com
+        return_code=$?
+        ((ping_times=ping_times+1))
+        if [ $return_code -ne 0 ]; then
+                echo "ping times : $ping_times"
+                sleep 4s
+        fi
+done
+
 #reboot
 sleep 3s
 python $2/main.py > log.txt &
